d6644d55aab0fc63b41fe78d1c83e0533c167106
Christian Lete <christian.lete+github@gmail.com>
2023-08-17 18:20:36 +0200

    startup script

--- terraform/web-tocos.tf
+++ terraform/web-tocos.tf
@@ -4,6 +4,23 @@ resource "aws_instance" "web" {
   subnet_id                   = aws_subnet.tocos-public_subnet.id
   vpc_security_group_ids      = [aws_security_group.tocos-security_group.id]
   associate_public_ip_address = true
+  key_name = "pepino"
+  ser_data = <<-EOL
+  #!/bin/bash -xe
+
+   apt update
+   apt-get -y install ca-certificates curl gnupg
+   install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+   chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+   apt-get update 
+   apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+   dockerd & 
+  EOL
 
   tags = {
     Name = "Tocos-web"
@@ -21,7 +38,7 @@ provider "aws" {
     apigateway     = "http://localhost:4567"
     cloudformation = "http://localhost:4581"
     cloudwatch     = "http://localhost:4582"
-    dynamodb       = "http://localhost:4569"
+    #dynamodb       = "http://localhost:4569"
     es             = "http://localhost:4578"
     firehose       = "http://localhost:4573"
     iam            = "http://localhost:4593"
@@ -31,10 +48,10 @@ provider "aws" {
     redshift       = "http://LOCALHOST:4577"
     s3             = "http://localhost:4572"
     secretsmanager = "http://localhost:4584"
-    ses            = "http://localhost:4579"
-    sns            = "http://localhost:4575"
-    sqs            = "http://localhost:4576"
-    ssm            = "http://localhost:4583"
+    #ses            = "http://localhost:4579"
+    #sns            = "http://localhost:4575"
+    #sqs            = "http://localhost:4576"
+    #ssm            = "http://localhost:4583"
     stepfunctions  = "http://localhost:4585"
     sts            = "http://localhost:4592"
     ec2            = "http://localhost:4597"
